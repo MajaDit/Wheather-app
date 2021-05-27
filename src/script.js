966ca3200dd61a966d80478d79fd4da1722d6f56
MajaDit <marie.ditrtova@gmail.com>
2021-05-27 14:49:19 +0100

    added units to url

--- src/script.js
+++ src/script.js
@@ -1,3 +1,10 @@
 let apiKey="ad2ff494a03279abf350d133542944f6";
 let city ="London"
-let apiUrl="api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}";
+let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+function displayCurrentTemperature(response){
+  console.log(timestamp);
+}
+console.log(apiUrl);
+axios.get(apiUrl).then(displayCurrentTemperature);
+
