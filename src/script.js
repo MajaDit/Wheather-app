6e4eb3c6d9ea50243f0ce218539b6911c005ecd0
MajaDit <marie.ditrtova@gmail.com>
2021-05-27 15:57:31 +0100

    formated date and time

--- src/script.js
+++ src/script.js
@@ -2,9 +2,21 @@ let apiKey="ad2ff494a03279abf350d133542944f6";
 let city ="London"
 let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-function displayCurrentTemperature(response){
-  console.log(timestamp);
+function formatDate(timestamp){
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10){hours=`0${hours}`};
+  let minutes = date.getMinutes();
+  if (minutes <10){minutes=`0${minutes}`};
+  let day = date.getDate();
+  let months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+  let month = months[date.getMonth()];
+  return ` ${day} ${month} ${hours}:${minutes}`;
 }
-console.log(apiUrl);
-axios.get(apiUrl).then(displayCurrentTemperature);
+function displayDate(response){
+  console.log(response.data);
+  let date= document.querySelector("#date");
+  date.innerHTML=formatDate(response.data.dt *1000);
+}
 
+axios.get(apiUrl).then(displayDate);
