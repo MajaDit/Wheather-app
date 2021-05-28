14e9e407a33df1e042cc4c074eea814bb23790ea
MajaDit <marie.ditrtova@gmail.com>
2021-05-28 12:00:27 +0100

    cleaned date and time function

--- src/script.js
+++ src/script.js
@@ -1,8 +1,4 @@
-  let apiKey="ad2ff494a03279abf350d133542944f6";
-  let city ="London"
-  let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-function formatDate(timestamp){
+  function formatDate(timestamp){
   let date = new Date(timestamp);
   let hours = date.getHours();
   if (hours < 10){hours=`0${hours}`};
@@ -13,12 +9,7 @@ function formatDate(timestamp){
   let month = months[date.getMonth()];
   return ` ${day} ${month} ${hours}:${minutes}`;
 }
-function displayDate(response){
-  let date= document.querySelector("#date");
-  date.innerHTML=formatDate(response.data.dt *1000);
-}
 
-axios.get(apiUrl).then(displayDate);
 
 function findCurrentData(response){
   let currentTemperature = document.querySelector("#current-temperature");
@@ -32,6 +23,8 @@ function findCurrentData(response){
   humidity.innerHTML=`${Math.round(response.data.main.humidity)}%`;
   let wind=document.querySelector("#wind");
   wind.innerHTML=`${response.data.wind.speed}km/h`;
+  let date= document.querySelector("#date");
+  date.innerHTML=formatDate(response.data.dt *1000);
   console.log(response.data);
 }
 function findCity(city){
