562d83bcd55240368fedf651b888388482d3e638
MajaDit <marie.ditrtova@gmail.com>
2021-06-02 17:05:35 +0100

    got daily forecast response from api

--- src/script.js
+++ src/script.js
@@ -10,7 +10,8 @@
   return ` ${day} ${month} ${hours}:${minutes}`;
 }
 
-function displayForecast(){
+function displayForecast(response){
+  console.log(response.data.daily);
   let forecastElement=document.querySelector("#forecast");
   let forecastHTML= `<div class="row">`;
   let days = ["Wed", "Thu", "Fri", "Sat", "Sun", "Mon"];
@@ -25,6 +26,12 @@ function displayForecast(){
     forecastHTML= forecastHTML + `</div>`;
 
   forecastElement.innerHTML= forecastHTML;
+}
+function getForecast(coordinates){
+  let apiKey="ad2ff494a03279abf350d133542944f6";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayForecast);
 }
 function findCurrentData(response){
   let currentTemperature = document.querySelector("#current-temperature");
@@ -42,6 +49,8 @@ function findCurrentData(response){
   celsiusTemperature=Math.round(response.data.main.temp);
   let icon = document.querySelector("#icon");
   icon.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+
+  getForecast(response.data.coord);
 }
 function findCity(city){
   let apiKey="ad2ff494a03279abf350d133542944f6";
@@ -82,5 +91,4 @@ function displayCelsius(event){
 let celsius=document.querySelector("#celsius");
 celsius.addEventListener("click", displayCelsius);
 
-displayForecast();
 findCity("London");
